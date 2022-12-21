f03a483c8efc1bbdd6e9bf3ae6341dade01b3d09
juju1321 <venera_2022@mail.ru>
2022-12-21 12:46:38 +0300

    add menu under the card

--- src/component/card/card.js
+++ src/component/card/card.js
@@ -1,4 +1,5 @@
 import { createElemetns } from "../../utils/createElemet.js";
+import { popupElement } from '../popup/popup.js'
 
 export const renderElem = (element) => {
   const root = document.querySelector('.cards-wrapper')
@@ -37,4 +38,78 @@ export const renderElem = (element) => {
     innerHTML: text,
  })
   cardDetails.appendChild(imgDescription);
+
+
+  // menu под картинкой
+  const buttonWrapper = createElemetns('div' , {
+    className: "button-wrapper",
+  })
+  imgWrapper.appendChild(buttonWrapper);
+
+  const buttonShowd = createElemetns('div', {
+    className: 'button-showd',
+  })
+  buttonWrapper.appendChild(buttonShowd);
+
+  const menuDots = createElemetns('div', {
+    className: 'menuDots',
+  })
+  buttonShowd.appendChild(menuDots);
+
+  const dots = createElemetns('div' , {
+    className: 'dots',
+  })
+  menuDots.appendChild(dots)
+
+  const menuCards = createElemetns('div', {
+    className: 'menu__cards',
+  })
+  buttonWrapper.appendChild(menuCards)
+
+  const menuTitle = createElemetns('p', {
+    className: 'menu__title',
+    innerHTML: 'Этот пин похож на те, которые вы недавно просматривали'
+  })
+  menuCards.appendChild(menuTitle)
+
+  const cardsMenu = createElemetns('div', {
+    className: 'cards__menu',
+  })
+  menuCards.appendChild(cardsMenu)
+
+  const menuListCards = createElemetns('ul', {
+    className: 'menu__list-card',
+  })
+  cardsMenu.appendChild(menuListCards)
+
+  const menuList = [
+    {
+      id: 1,
+      innerHTML: 'Добавить на доску'
+    },
+
+    {
+      id: 2,
+      innerHTML: 'Пожаловаться'
+    }
+  ]
+
+  const renderMenuList = (elem) => {
+    const {innerHTML} = elem
+
+    const menuItemCard = createElemetns('li', {
+      className: 'menu__item-card',
+      innerHTML: innerHTML,
+    })
+    menuListCards.appendChild(menuItemCard)
+  }
+
+  menuList.forEach(elem => {
+    renderMenuList(elem)
+  })
+
+  menuDots.addEventListener('click', () => {
+    menuCards.style.display = 'flex', 
+    menuCards.style.flexDirection = 'column'
+  })
 }
--- src/component/popup/popup.js
+++ src/component/popup/popup.js
@@ -31,3 +31,25 @@ const buttonAddDeskThree = createElemetns('button', {
 buttonAll.appendChild(buttonAddDeskThree)
 
 
+// import { createElemetns } from "../../utils/createElemet.js";
+
+// export const popupElement = (element) => {
+//     const popupBtn = document.querySelector('.popup')
+
+//     const popupListArr = ['Desk 1', 'Desk 2', 'Desk 3']
+//     const listUl = document.createElement('ul')
+//     listUl.className = 'popup__list'
+//     popupBtn.appendChild(listUl)
+//     document.querySelector('.popup__list').hidden = true
+
+//     for (let i = 0; i < popupListArr.length; i++) {
+//         let li = document.createElement('li')
+//         const linkDesk = createElemetns('a', {
+//           href: '#',
+//           className: 'popup__item',
+//           innerText: popupListArr[i]
+//         })
+//         li.appendChild(linkDesk)
+//         listUl.appendChild(li);
+//       };
+// }
